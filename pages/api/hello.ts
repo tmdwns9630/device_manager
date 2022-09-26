499a83d6992afea72b9c1e3c7663ca071a6e5c52
CSJ <tmdwns9630@gmail.com>
2022-09-26 13:24:23 +0900

    README.md에 세팅 방법 작성 메모한 주석 내용은 MyNote.md에 작성했다.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }
+
+//이전까지 req, res는 내부 함수나 변수의 자동 완성 리스트가 안 떴다.
+// 이제는 함수 포맷 정보까지 편하게 나온다.
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Counter from "../components/Counter";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <div className="">Hello World</div>;
+  return (
+    <>
+      <div className="">Hello World</div>
+      <Counter title={[1, 2, 3, 4, 5]} subtitle="타입스크립트" />
+    </>
+  );
 };
 
 export default Home;
